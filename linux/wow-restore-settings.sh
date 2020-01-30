da948ec2403da890d05e87ae1860ed5ac84ee44e
Daniel Silva <daniel@pop-os.localdomain>
2020-01-30 14:29:07 -0500

    Added Cache to something we bring over from Windows; Backing up Config.wtf ahead of restoration to preserve that.

--- linux/wow-restore-settings.sh
+++ linux/wow-restore-settings.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# mount windows drive
-~/bin/mount-windows.sh
+# Assumes Windows drive is mounted to /mnt/windows
+
+# Backup Linux Config.wtf
+cp ~/Games/battlenet/drive_c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/WTF/Config.wtf /tmp/Config.wtf
 
 # remove existing files and copy windows files
+rm -rf ~/Games/battlenet/drive_c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/Cache
+cp -R /mnt/windows/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/Cache ~/Games/battlenet/drive_c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/
 rm -rf ~/Games/battlenet/drive_c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/Interface
 cp -R /mnt/windows/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/Interface ~/Games/battlenet/drive_c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/
 rm -rf ~/Games/battlenet/drive_c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/WTF 
 cp -R /mnt/windows/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/WTF ~/Games/battlenet/drive_c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/
+
+# Restore Linux Config.wtf
+cp /tmp/Config.wtf ~/Games/battlenet/drive_c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/WTF/Config.wtf
--- windows/wow-restore-settings.sh
+++ windows/wow-restore-settings.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# backup Windows Config.wtf
+cp /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/WTF/Config.wtf /tmp/Config.wtf
+
 # clear out current settings
 echo -n " > Clearing out current settings... "
 rm -rf /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/Interface
@@ -17,6 +20,9 @@ unzip -q ./wow.zip
 mv ./Interface /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/
 mv ./WTF /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/
 echo "DONE"
+
+# restore Windows Config.wtf
+cp /tmp/Config.wtf /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/WTF/Config.wtf
 
 # clean
 echo -n " > Removing /tmp/wow.zip... "
