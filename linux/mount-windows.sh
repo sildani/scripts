6defed7a9f715507cb14c1f30f5124be6efdd3b4
Daniel Silva <daniel@pop-os.localdomain>
2020-01-30 14:28:03 -0500

    Refined how we mount the Windows drive.

--- linux/mount-windows.sh
+++ linux/mount-windows.sh
@@ -2,4 +2,8 @@
 
 # Mount Windows drive in read-only mode to prevent data corruption. Edit for the appropriate device for you. Requires sudo access.
 
-sudo mount -o ro /dev/nvme0n1p4 /mnt/windows
+FILE=/mnt/windows
+if [ ! -f "$FILE" ]; then
+  sudo mkdir -p $FILE
+fi
+sudo mount -o ro /dev/nvme1n1p4 $FILE
