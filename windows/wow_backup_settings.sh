ec55ae9f747316b7813839b19c7e44a3de6f1f85
Daniel Silva <daniel@silvanolte.com>
2021-02-16 08:37:19 -0500

    Changed backup impl to copy to local Google Drive

--- windows/wow_backup_settings.sh
+++ windows/wow_backup_settings.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
+# Assumes google drive is available in /d/google\ drive/
+
 FILENAME=wow.zip
-FILE=/tmp/$FILENAME
+SOURCE_FILE=/tmp/$FILENAME
+TARGET_FILE="/d/google drive/$FILENAME"
+
+# echo -n "FILE="
+# echo $SOURCE_FILE
+# echo -n "TARGET_FILE="
+# echo $TARGET_FILE
 
 # remove previous archive if exists
-if [ -f "$FILE" ]; then
+if [ -f "$SOURCE_FILE" ]; then
   echo -n " > Removing previous archive file..."
-  rm $FILE
+  rm $SOURCE_FILE
   echo " DONE"
 fi
 
 # create the zip file using GNU zip (see https://github.com/bmatzelle/gow/wiki for the win32 binary of the GNU zip)
 echo -n " > Creating archive..."
 cd /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_
-zip -rq $FILE Interface WTF
+zip -rq $SOURCE_FILE Interface WTF
 echo " DONE"
 
 # upload the zip file
-# see https://github.com/andreafabrizi/Dropbox-Uploader
-dropbox_uploader delete $FILENAME
-dropbox_uploader upload $FILE $FILENAME
+echo -n " > Uploading archive..."
+rm -f $TARGET_FILE
+cp $SOURCE_FILE "$TARGET_FILE"
+echo " DONE"
 
 # clean up file
 echo -n " > Cleanup ..."
-rm $FILE
+rm $SOURCE_FILE
 echo " DONE"
