b67208c1d9db38d1a7835b142f411e5e61e72bac
Daniel Silva <daniel@silvanolte.com>
2021-02-16 18:15:13 -0500

    Updated the restore impl to read from local Google Drive

--- windows/wow_restore_settings.sh
+++ windows/wow_restore_settings.sh
@@ -1,30 +1,55 @@
 #!/bin/bash
 
+# Assumes google drive is available in /d/google\ drive/
+
+WOW_DIR="/c/Program Files (x86)/World of Warcraft/_retail_"
+CONFIG_FILE="$WOW_DIR/WTF/Config.wtf"
+SOURCE_FILE="/d/google drive/wow.zip"
+TEMP_DIR="/tmp/addon_backup"
+
+# echo -n "WOW_DIR=" ; echo $WOW_DIR
+# echo -n "CONFIG_FILE=" ; echo $CONFIG_FILE
+# echo -n "SOURCE_FILE=" ; echo $SOURCE_FILE
+# echo -n "TEMP_DIR=" ; echo $TEMP_DIR
+
+# setup the temp dir to work in
+echo -n " > Creating the temp dir... "
+if [ -d "$TEMP_DIR" ]; then
+  rm -rf $TEMP_DIR
+fi
+mkdir $TEMP_DIR
+echo "DONE"
+
 # backup game settings
-cp /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/WTF/Config.wtf /tmp/Config.wtf
-
-# clear out current settings
-echo -n " > Clearing out current settings... "
-rm -rf /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/Interface
-rm -rf /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/WTF
+echo -n " > Backing up Config.wtf... "
+cp "$CONFIG_FILE" $TEMP_DIR/
 echo "DONE"
 
-# download the zip file
-# see https://github.com/andreafabrizi/Dropbox-Uploader
-dropbox_uploader download /wow.zip /tmp/wow.zip
+# back up current local settings
+echo -n " > Backing up current local addon settings... "
+mkdir $TEMP_DIR/local_backup
+cp -r "$WOW_DIR/Interface" $TEMP_DIR/local_backup/
+cp -r "$WOW_DIR/WTF" $TEMP_DIR/local_backup/
+echo "DONE"
 
-# unpack
-echo -n " > Unpacking archive and moving into location... "
-cd /tmp
+# get into the temp dir to get to work
+cd $TEMP_DIR
+echo -n " > Change dirs... "
+echo `pwd`
+
+# stage the backed up data
+echo -n " > Staging the cloud back-up settings to restore... "
+cp "$SOURCE_FILE" ./
 unzip -q ./wow.zip
-mv ./Interface /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/
-mv ./WTF /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/
 echo "DONE"
 
-# restore game settings
-cp /tmp/Config.wtf /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/WTF/Config.wtf
-
-# clean
-echo -n " > Removing /tmp/wow.zip... "
-rm /tmp/wow.zip
+# move the files into place
+echo -n " > Restoring the cloud back-up and original Config.wtf..."
+rm -rf "$WOW_DIR/Interface"
+rm -rf "$WOW_DIR/WTF"
+cp -r ./Interface "$WOW_DIR/"
+cp -r ./WTF "$WOW_DIR/"
+cp ./Config.wtf "$CONFIG_FILE"
 echo "DONE"
+
+echo "" ; echo "JOB DONE!"
