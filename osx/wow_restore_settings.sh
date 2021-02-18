7143c06df3fdf97079f8e91659443896c827a094
Daniel Silva <daniel@silvanolte.com>
2021-02-18 16:22:18 -0500

    Updated the restore settings impl for osx

--- osx/wow_restore_settings.sh
+++ osx/wow_restore_settings.sh
@@ -4,25 +4,32 @@
 
 SOURCE_DIR=$(cd ~/Downloads; pwd)
 SOURCE_FILE=$SOURCE_DIR/wow.zip
+TEMP_DIR=/tmp/wow_restore_settings
+STAGED_SOURCE_FILE=$TEMP_DIR/wow.zip
+WOW_DIR="/Applications/World of Warcraft/_retail_"
 
 printf " [wow_restore_settings] > SOURCE_DIR=$SOURCE_DIR\n"
 printf " [wow_restore_settings] > SOURCE_FILE=$SOURCE_FILE\n"
+printf " [wow_restore_settings] > TEMP_DIR=$TEMP_DIR\n"
+printf " [wow_restore_settings] > STAGED_SOURCE_FILE=$STAGED_SOURCE_FILE\n"
+printf " [wow_restore_settings] > WOW_DIR=$WOW_DIR\n"
+
 
 if [ -f "$SOURCE_FILE" ]; then
   printf " [wow_restore_settings] > Restoring settings from $SOURCE_FILE... "
-  if [ -d /tmp/wow_restore_settings ]; then
-    rm -rf /tmp/wow_restore_settings
+  if [ -d $TEMP_DIR ]; then
+    rm -rf $TEMP_DIR
   fi
-  mkdir /tmp/wow_restore_settings
-  cd /tmp/wow_restore_settings
-  cp ~/Downloads/wow.zip .
-  unzip -q wow.zip
+  mkdir $TEMP_DIR
+  cd $TEMP_DIR
+  cp $SOURCE_FILE $STAGED_SOURCE_FILE
+  unzip -q $STAGED_SOURCE_FILE
   mkdir ./current_local_settings
-  mv /Applications/World\ of\ Warcraft/_retail_/Interface ./current_local_settings/
-  mv /Applications/World\ of\ Warcraft/_retail_/WTF ./current_local_settings
-  cp -r ./Interface /Applications/World\ of\ Warcraft/_retail_
-  cp -r ./WTF /Applications/World\ of\ Warcraft/_retail_
-  cp ./current_local_settings/WTF/Config.wtf /Applications/World\ of\ Warcraft/_retail_/WTF/
+  mv "$WOW_DIR/Interface" ./current_local_settings/
+  mv "$WOW_DIR/WTF" ./current_local_settings/
+  cp -r ./Interface "$WOW_DIR/"
+  cp -r ./WTF "$WOW_DIR/"
+  cp ./current_local_settings/WTF/Config.wtf "$WOW_DIR/WTF/"
   printf "DONE\n"
 else
   printf " [wow_restore_settings] > $SOURCE_FILE is not found, exiting\n"
